cca488675cf9bbb0471f101d99b688a8f8f76f34
Tonio1999 <44210786+Tonio1999@users.noreply.github.com>
2019-12-06 20:15:07 -0500

    Update shell.c

--- shell.c
+++ shell.c
@@ -12,72 +12,61 @@ char **environ;
 
 int main() 
 {
-    // Stores the string typed into the command line.
     char command_line[MAX_COMMAND_LINE_LEN];
-
-    // Stores the tokenized command line input.
     char *arguments[MAX_COMMAND_LINE_ARGS];
+    pid_t pid;
     
     while (true) 
     {
-        // Print the shell prompt.
         printf("%s", prompt);
         fflush(stdout);
-        
-        // Read input from stdin and store it in command_line. If there's an
-        // error, exit immediately. (If you want to learn more about this line,
-        // you can Google "man fgets")
         if ((fgets(command_line, MAX_COMMAND_LINE_LEN, stdin) == NULL) && ferror(stdin)) 
         {
             fprintf(stderr, "fgets error");
             exit(0);
         }
-        // If the user input was EOF (ctrl+d), exit the shell.
         if (feof(stdin)) 
         {
             printf("\n");
             fflush(stdout);
             fflush(stderr);
             return 0;
-        }      
-        // TODO:
-        // 1. Tokenize the command line input (split it on whitespace)
-        int index = 0;
-        arguments[index] = strtok(command_line, delimiters);
-        while (arguments[index] != NULL)
-        {
-          arguments[index] = strtok(command_line, delimiters);
-          index++;
         }
-        int chld = fork();
-        if(chld == 0) 
+        char *command;
+        char *arg;
+        int i = 0;  
+        command = strtok(command_line,delimiters);
+        arg = strtok(NULL,delimiters);
+        while (arg != NULL)
         {
-          if(execve(arguments[0], arguments, environ) == -1)
-          {
-            printf("%s: Command not found.\n", arguments[0]);
-            return -1;
-           }
-          return 0;
-        } 
-        else if(chld > 0)
+          arguments[i] = arg;
+          i+=1;
+          arg = strtok(NULL,delimiters);
+        }
+        pid = fork();
+        
+        if (pid < 0)
         {
-          wait(NULL);
-        } else 
-        {
-          return -1;
+            perror("Fork error!\n"); 
+          exit(1);
         }
       
-        // 2. Create a child process which will execute the command line input
-        // 3. The parent process should wait for the child to complete
-        // 
-        // Hints (put these into Google):
-        // man fork
-        // man execve
-        // man wait
-        // man strtok
+        else if (pid == 0)
+        {
+          printf("This is the Child process!\n");
+          if (execve(command, arguments, environ) < 0 )
+          {
+            perror("Execution error!\n");   
+            exit(1); 
+          }
+          exit(0);
+        }
+        else 
+        {
+          printf("This is the Parent process!\n");
+          wait(NULL);
+          printf("Child has terminated.\n");
+        }
     }
-    
-    // This should never be reached.
     return -1;
 }
-
